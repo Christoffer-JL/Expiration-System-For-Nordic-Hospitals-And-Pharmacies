18ef80d343f7f46296f6d646ffdb534e53eefb97
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-10-06 17:08:39 +0200

    Kotlin Gradle version updated to 1.9.10, Android minSdkVersion set to 21, added QR API, added test code for API

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,29 +1,61 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'widgets/custom_button.dart'; // Import your custom button widget
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+MobileScannerController cameraController = MobileScannerController();
 
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Start Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Welcome to the Start Screen'),
-            SizedBox(height: 20), // Add some spacing
-            CustomButton(
-              text: 'button example',
-              onPressed: () {
-                // Add your button click logic here
-                Navigator.pushNamed(
-                    context, '/qr_scan'); // Navigate to the QR scanning screen
+        title: const Text('Mobile Scanner'),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
               },
             ),
-          ],
-        ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.toggleTorch(),
+          ),
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.cameraFacingState,
+              builder: (context, state, child) {
+                switch (state as CameraFacing) {
+                  case CameraFacing.front:
+                    return const Icon(Icons.camera_front);
+                  case CameraFacing.back:
+                    return const Icon(Icons.camera_rear);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.switchCamera(),
+          ),
+        ],
+      ),
+      body: MobileScanner(
+        // fit: BoxFit.contain,
+        controller: cameraController,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
+          }
+        },
       ),
     );
   }
