e421b1d61a4b146036be37ed9b30276449af469f
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-10-14 13:46:59 +0200

    Updated credentails

--- ServerApp.js
+++ ServerApp.js
@@ -1,12 +1,12 @@
 /*
-*
-* Kör servern med "node ServerApp.js"
-*
-*/
+ *
+ * Kör servern med "node ServerApp.js"
+ *
+ */
 
 // Import required modules
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // Create an Express application
 const app = express();
@@ -15,29 +15,26 @@ const app = express();
 app.use(bodyParser.json());
 
 // Handle GET requests to /api-endpoint
-app.get('/api-endpoint', (req, res) => {
+app.get("/api-endpoint", (req, res) => {
   // Handle GET requests here (e.g., retrieve data)
-  res.send('This is a GET request to /api-endpoint');
+  res.send("This is a GET request to /api-endpoint");
 });
 
 // Handle POST requests to /api-endpoint
-app.post('/api-endpoint', (req, res) => {
-
+app.post("/api-endpoint", (req, res) => {
   const data = req.body.data;
   insertData(data);
   // Insert 'data' into your MySQL database here
   // Respond to the client accordingly
-  res.status(200).json({ message: 'Data received and processed.' });
+  res.status(200).json({ message: "Data received and processed." });
 });
 
-app.post('/api-endpoint2', (req, res) => {
+app.post("/api-endpoint2", (req, res) => {
   const data = req.body.data;
   // Insert 'data' into table 2 here
-  res.status(200).json({ message: 'Data received and processed for table 2.' });
+  res.status(200).json({ message: "Data received and processed for table 2." });
   insertData2(data);
 });
-
-
 
 // Start the server
 const port = process.env.PORT || 3000; // Use the specified port or 3000 as a default
@@ -45,52 +42,52 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: 'sql11.freesqldatabase.com',
-  user: 'sql11648708',
-  password: 'bYlA6x4JHr',
-  database: 'sql11648708',
-  port: 3306
+  host: "sql11.freesqldatabase.com",
+  user: "sql11652098",
+  password: "X1j3W3Q5Ir",
+  database: "sql11652098",
+  port: 3306,
 });
 
 // Connect to MySQL
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error("Error connecting to MySQL:", err);
   } else {
-    console.log('Connected to MySQL');
+    console.log("Connected to MySQL");
   }
 });
 
 // Insert data into MySQL
 function insertData(data) {
-    console.log(data);
-    // Split the data into an array of values, assuming it's a comma-separated string
-    const values = data.split(',');
-  
-    const sql = 'INSERT INTO dummy_table (name, age) VALUES (?, ?)';
-    db.query(sql, values, (err, result) => {
-      if (err) {
-        console.error('Error inserting data:', err);
-      } else {
-        console.log('Data inserted successfully:', result);
-      }
-    });
-  }
-  
-  function insertData2(data) {
-    console.log(data);
-    // Split the data into an array of values, assuming it's a comma-separated string
-    const values = data.split(',');
-  
-    const sql = 'INSERT INTO dummy_table2 (name, age) VALUES (?, ?)';
-    db.query(sql, values, (err, result) => {
-      if (err) {
-        console.error('Error inserting data:', err);
-      } else {
-        console.log('Data inserted successfully:', result);
-      }
-    });
-  }
+  console.log(data);
+  // Split the data into an array of values, assuming it's a comma-separated string
+  const values = data.split(",");
+
+  const sql = "INSERT INTO dummy_table (name, age) VALUES (?, ?)";
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error inserting data:", err);
+    } else {
+      console.log("Data inserted successfully:", result);
+    }
+  });
+}
+
+function insertData2(data) {
+  console.log(data);
+  // Split the data into an array of values, assuming it's a comma-separated string
+  const values = data.split(",");
+
+  const sql = "INSERT INTO dummy_table2 (name, age) VALUES (?, ?)";
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error inserting data:", err);
+    } else {
+      console.log("Data inserted successfully:", result);
+    }
+  });
+}
