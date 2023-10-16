85d6d86e7f2e424b020db76871952abd23895e54
Zm4in Z <argtimz@gmail.com>
2023-10-16 22:23:21 +0200

    image for buttons

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,40 +9,62 @@ class StartScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Start Screen'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 20),
-            Container(
-              width: 216,
-              height: 76,
-              child: CustomButton(
-                text: 'Registrera',
-                onPressed: () {
-                  Navigator.pushNamed(context, '/qr_scan'); // Navigate to the QR scanning screen
-                },
-                color: Color(0xFF73C9DF),
-                fontSize: 25,
-              ),
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 20),
+                Container(
+                  width: 216,
+                  height: 76,
+                  child: CustomButton(
+                    text: 'Registrera',
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/qr_scan');
+                    },
+                    color: Color(0xFF73C9DF),
+                    fontSize: 25,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 216,
+                  height: 76,
+                  child: CustomButton(
+                    text: 'Katalog',
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/catalog_start');
+                    },
+                    color: Color(0xFFFDDD41),
+                    fontSize: 25,
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            Container(
-              width: 216,
-              height: 76,
-              child: CustomButton(
-                text: 'Katalog',
-                onPressed: () {
-                  Navigator.pushNamed(context, '/catalog_start'); // Navigate to the QR scanning screen
-                },
-                color: Color(0xFFFDDD41),
-                fontSize: 25,
-              ),
+          ),
+          Positioned(
+            top: 290, // Adjust the top position as needed
+            left: 310, // Adjust the left position as needed
+            child: Image.asset(
+              'assets/scanner.png',
+              width: 50,
+              height: 50,
             ),
-          ],
-        ),
+          ),
+          Positioned(
+            top: 390, // Adjust the top position as needed
+            left: 310, // Adjust the left position as needed
+            child: Image.asset(
+              'assets/catalog.png',
+              width: 50,
+              height: 50,
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
