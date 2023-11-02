4daf84e1183f3a96c5b7935abaaa97f034fb5d08
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-11-02 21:53:49 +0100

    Added modularity to Ip config file

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,5 @@
 class AppConfig {
-  static const String apiUrl = 'http://localhost:3000';
+  static const String apiIp = '127.0.0.1';
+  static const String apiPort = '3000';
+  static const String apiUrl = 'http://${apiIp}:${apiPort}';
 }
