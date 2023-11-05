0471d374906f16c087c87dcd9845d61d40d5c6e2
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-11-05 19:18:09 +0100

    Begun work on expiration screen

--- lib/pages/catalog_expiration_screen.dart
+++ lib/pages/catalog_expiration_screen.dart
@@ -1,60 +1,15 @@
 import 'package:flutter/material.dart';
-import '../widgets/pop_up.dart';
 
-class CatalogExpirationScreen extends StatelessWidget {
+class CatalogExpirationScreen extends StatefulWidget {
   const CatalogExpirationScreen({super.key});
 
-  void _showPopup(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return PopUp(
-          title: 'Popup Title',
-          content: 'Popup Content',
-          buttonText1: 'Cancel',
-          buttonText2: 'OK',
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        );
-      },
-    );
-  }
+  @override
+  CatalogExpirationScreenState createState() => CatalogExpirationScreenState();
+}
 
+class CatalogExpirationScreenState extends State<CatalogExpirationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(0),
-        child: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-        ),
-      ),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              color: Colors.blue,
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ),
-          const Center(
-            child: Text(
-              'Expiration screen',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
