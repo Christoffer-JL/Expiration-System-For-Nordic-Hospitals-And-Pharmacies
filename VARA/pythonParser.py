37e26051b745c3ef84dc2a48c9fbd607e7b44cc9
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-11-06 21:56:28 +0100

    WIP: The VARA-application now commits the data to the DB, however seems to crash randomly

--- VARA/pythonParser.py
+++ VARA/pythonParser.py
@@ -3,8 +3,8 @@ import paramiko
 import zipfile
 import os
 import shutil
+import mysql.connector
 
-# Define the namespace mapping
 namespaces = {
     'asab': 'urn:schemas-asab:vara-export',
     'def': 'urn:schemas-asab:definitioner',
@@ -12,18 +12,15 @@ namespaces = {
     'lxd': 'urn:schemas-asab:lexikondata',
 }
 
-# SFTP Configuration
 sftp_host = 'sftp.ehalsomyndigheten.se'
 sftp_port = 22
 sftp_user = 'pe465'
-sftp_password = 'ZaGa8K\\Z[M(#'  # Note the double backslash to escape the special characters
+sftp_password = 'ZaGa8K\\Z[M(#'
 sftp_remote_dir = '6'
 remote_zip_file = 'vara-export-6_20231106_003156.zip'
 
-# Local directory to unpack the ZIP file
 local_unzip_dir = 'unzip_folder'
 
-# Create a Paramiko SSH client and SFTP client
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -31,42 +28,59 @@ try:
     ssh.connect(sftp_host, port=sftp_port, username=sftp_user, password=sftp_password)
     sftp = ssh.open_sftp()
 
-    # Change to the remote directory and get the ZIP file
     sftp.chdir(sftp_remote_dir)
     sftp.get(remote_zip_file, remote_zip_file)
 
-    # Unzip the downloaded ZIP file
     with zipfile.ZipFile(remote_zip_file, 'r') as zip_ref:
         zip_ref.extractall(local_unzip_dir)
 
-    # Parse the XML file from the unzipped folder
     xml_file_path = os.path.join(local_unzip_dir, 'vara-export', 'lm-data.xml')
     print(xml_file_path)
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
 
-    # Iterate through each 'def:lmprodukt' element
-    for lmprodukt in root.findall('.//def:lmprodukt', namespaces=namespaces):
-        produktnamn = lmprodukt.find('./def:produktnamn', namespaces=namespaces).text
-        varunummer_element = lmprodukt.find('.//def:artikel_id[@artikel_id_typ="nordiskt-varunr"]', namespaces=namespaces)
-        varunummer = varunummer_element.attrib['id_varde'] if varunummer_element is not None else None
-        forpackning_text_element = lmprodukt.find('.//def:forpackning_text', namespaces=namespaces)
-        forpackning_text = forpackning_text_element.text if forpackning_text_element is not None else None
-        streckkod_element = lmprodukt.find('.//def:artikel_id[@artikel_id_typ="streck-kod"]', namespaces=namespaces)
-        streckkod = streckkod_element.attrib['id_varde'] if streckkod_element is not None else None
+    db_config = {
+        "host": "sql11.freesqldatabase.com",
+        "user": "sql11653843",
+        "password": "ri3ZrApRl4",
+        "database": "sql11653843",
+        "port": 3306,
+    }
 
-        # Check if any of the values is not "N/A" before printing
-        if varunummer is not None and forpackning_text is not None and streckkod is not None:
-            # Do something with the extracted data, e.g., print it
-            print(f"Produktnamn: {produktnamn}")
-            print(f"Varunummer: {varunummer}")
-            print(f"Förpackning: {forpackning_text}")
-            print(f"Produktkod: {streckkod}")
+    try:
+        cnx = mysql.connector.connect(**db_config)
+        cursor = cnx.cursor()
 
-        # Close the SFTP connection
+        for lmprodukt in root.findall('.//def:lmprodukt', namespaces=namespaces):
+
+            produktnamn = lmprodukt.find('./def:produktnamn', namespaces=namespaces).text
+            varunummer_element = lmprodukt.find('.//def:artikel_id[@artikel_id_typ="nordiskt-varunr"]', namespaces=namespaces)
+            varunummer = varunummer_element.attrib['id_varde'] if varunummer_element is not None else None
+            forpackning_text_element = lmprodukt.find('.//def:forpackning_text', namespaces=namespaces)
+            forpackning_text = forpackning_text_element.text if forpackning_text_element is not None else None
+            streckkod_element = lmprodukt.find('.//def:artikel_id[@artikel_id_typ="streck-kod"]', namespaces=namespaces)
+            streckkod = streckkod_element.attrib['id_varde'] if streckkod_element is not None else None
+
+            if varunummer is not None and forpackning_text is not None and streckkod is not None:
+                insert_query = """
+                INSERT INTO Products (ProductCode, Packaging, NordicNumber, ArticleName)
+                VALUES (%s, %s, %s, %s)
+                ON DUPLICATE KEY UPDATE
+                Packaging = VALUES(Packaging), NordicNumber = VALUES(NordicNumber), ArticleName = VALUES(ArticleName)
+                """
+                data = (streckkod, forpackning_text, varunummer, produktnamn)
+                cursor.execute(insert_query, data)
+                
+            cnx.commit()
+
+    except mysql.connector.Error as err:
+        print(f"An error occurred while inserting data into the database: {err}")
+
+    finally:
+        cursor.close()
+        cnx.close()
+
     sftp.close()
-
-        # Clean up: Delete the local unzip folder
     os.remove(remote_zip_file)
     shutil.rmtree(local_unzip_dir)
 
@@ -74,5 +88,4 @@ except Exception as e:
     print(f"An error occurred: {str(e)}")
 
 finally:
-    # Close the SSH connection
     ssh.close()
