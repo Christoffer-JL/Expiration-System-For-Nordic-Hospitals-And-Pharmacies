f352f3f95cb4c6aa6e18f6243e5d5ffc038c29c3
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-11-12 13:48:18 +0100

    Made changes to ip config. Now supports rspi connection (albeit a bit broken)

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,11 @@
 class AppConfig {
-  static const String apiIp = '127.0.0.1';
+  static const String apiIp = IPAddresses.localHost;
   static const String apiPort = '3000';
   static const String apiUrl = 'http://${apiIp}:${apiPort}';
+}
+
+class IPAddresses {
+  static const String localHost = '127.0.0.1';
+  static const String externalPi = '188.150.228.31';
+  static const String internalPi = '192.168.0.105';
 }
