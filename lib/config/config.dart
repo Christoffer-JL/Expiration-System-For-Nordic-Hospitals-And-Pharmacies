b7692ccffa2bb7469bc43d95905764b110a472e6
khazari <khazarizhang@gmail.com>
2023-11-12 19:39:03 +0100

    some changes are made

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String apiIp = '192.168.0.4';
+  static const String apiIp = '127.0.0.1';
   static const String apiPort = '3000';
   static const String apiUrl = 'http://${apiIp}:${apiPort}';
 }
