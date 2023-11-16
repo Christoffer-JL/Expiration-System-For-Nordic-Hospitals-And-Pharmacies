6cf436cf4e20ce7bc4972ec9f093ab938b645548
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-11-16 22:03:34 +0100

    Modified config, removed comments from deployment script

--- lib/backend/deploy.sh
+++ lib/backend/deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Navigate to your project directory
 cd ~/reg_skane/Region-Sk-ne-2
 
-# Pull the latest changes from the Git repository
 git pull origin main
 
-# Restart your server application
 pm2 restart ServerApplet
 
