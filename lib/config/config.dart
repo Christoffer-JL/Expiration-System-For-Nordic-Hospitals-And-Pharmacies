c18299271f2ade7b21e235a93edbce41867895a1
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-11-16 17:13:19 +0100

    Added digitalOcean config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,15 +1,15 @@
 // Please do not push changes made to this file
 
 class AppConfig {
-  static const String apiIp = IPAddresses.externalPi;
+  static const String apiIp = IPAddresses.digitalOcean;
   static const String apiPort = '3000';
   static const String apiUrl = 'http://${apiIp}:${apiPort}';
 }
 
 class IPAddresses {
   static const String localHost = '127.0.0.1';
+  static const String digitalOcean = '104.248.81.249';
   static const String externalPi = '188.150.228.31';
   static const String internalPi = '192.168.0.105';
-  static const String computer =
-      '192.168.0.104'; // Run ipconfig and choose IPv4
+  static const String computer = ''; // Run ipconfig and choose IPv4
 }
