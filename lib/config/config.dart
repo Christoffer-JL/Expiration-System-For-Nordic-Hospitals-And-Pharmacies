3481be44df580abf9b5ec3cd16ed2d9f2c5a7b15
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-11-20 13:17:36 +0100

    Updated dummy data inserts, config file

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -7,11 +7,11 @@ class AppConfig {
 }
 
 class IPAddresses {
-  static const String DEPRECATEDlocalHost = '127.0.0.1';
+  static const String localHost = '127.0.0.1';
   static const String digitalOcean =
       '104.248.81.249'; // Webhook is live, so you can use this config. It auto deploys
   static const String DEPRECATEDexternalPi = '188.150.228.31';
   static const String DEPRECATEDinternalPi = '192.168.0.105';
-  static const String DEPRECATEDcomputer =
+  static const String computer =
       '192.168.1.172'; // Run ipconfig and choose IPv4
 }
