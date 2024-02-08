ca5de4637024ebca9cde2bf92bcc9175f87d9179
christoffer-JL <christoffer1917@hotmail.com>
2024-02-08 11:06:28 +0100

    Changed name of application

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PharmAware',
+      title: 'Expiration System',
       initialRoute: '/',
       routes: {
         '/': (context) => StartScreen(),
